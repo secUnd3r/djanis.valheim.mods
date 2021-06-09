f43d17f1e986072bdace4818945ef2bf5cbb1940
secUnd3r <84651338+secUnd3r@users.noreply.github.com>
2021-06-09 01:02:10 -0500

    Remade repo because I am bad at Git.

---
+++ djanis.valheim.silentping/SilentPing.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+
+namespace djanis.valheim.silentping
+{
+    [BepInPlugin("djanis.valheim.silentping", SilentPing.ModName, SilentPing.Version)]
+    public class SilentPing : BaseUnityPlugin
+    {
+        public const string Version = "1.0";
+        public const string ModName = "SilentPing";
+        Harmony _Harmony;
+        private static ConfigEntry<bool> modEnabled;
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+#if DEBUG
+            Log = Logger;
+#else
+            Log = new ManualLogSource(null);
+#endif
+            modEnabled = Config.Bind<bool>("General", "Enable Mod", true, "Enable mod to silence pings.");
+            Config.Save();
+
+            if (!modEnabled.Value)
+            {
+                return;
+            }
+            _Harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+
+        private void OnDestroy()
+        {
+            if (_Harmony != null)
+            {
+                _Harmony.UnpatchSelf();
+            }
+        }
+
+        [HarmonyPatch(typeof(Chat), nameof(Chat.AddString), typeof(string), typeof(string), typeof(Talker.Type))]
+
+        public static class Chat_RemovePingsWorldTexts_Patch
+        {
+            private static bool Prefix(ref Chat __instance, ref string user, ref string text, ref Talker.Type type)
+            {
+                if (modEnabled.Value && type == Talker.Type.Ping)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
